d8e0227fd4ac0483cfd4277d6c112f34ed129563
Obid Sharipov <obid.sharipov.80@mail.ru>
2021-05-16 11:23:37 +0500

    this is login first con

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
+  
 if (process.env.NODE_ENV !== 'praduction') {
     require('dotenv').config()
 }
-
-
 
 
 
@@ -14,10 +13,15 @@ const flash = require('express-flash')
 const passport = require('passport')
 
 const session = require('express-session')
-const initalizePassport = require('./passport-confeg')
+const initalizePassport = require('./passport-config')
+const methodOverrite=require('method-override')
+
+
 
 initalizePassport(passport,
-    email => users.find(user => user.email === email)
+    email => users.find(user => user.email === email),
+    id=>users.find(user => user.id === id)
+    
 )
 
 
@@ -31,30 +35,35 @@ app.use(session({
     saveUninitialized: false
 }))
 
+
+app.use(methodOverrite('_method'))
 app.use(passport.initialize())
+
+
 app.use(passport.session())
+
 
 app.set('view-engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: ' Olimajon ' })
-})
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+  })
 
-app.get('/login', (req, res) => {
+app.get('/login',checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
 })
-app.post('/login', passport.authenticate('local',{
+app.post('/login',checkNotAuthenticated, passport.authenticate('local',{
     successRedirect:'/',
     failureRedirect:'login',
     failureFlash:true
 }))
 
 
-app.get('/register', (req, res) => {
+app.get('/register',checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', checkNotAuthenticated,async (req, res) => {
     try {
         const hashPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
@@ -72,16 +81,28 @@ app.post('/register', async (req, res) => {
 
     }
 
-    console.log(users)
+ // console.log(users)
 })
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+  }
 
-
-
-
+  
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
 
 app.listen(pprt, () => {
     console.log(`men ${pprt}chi esshitim`)
 })
-
-
-   //davomi bor
